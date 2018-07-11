711fbc5627c1982b1ef6d27a614c034404cf4e45
RagtagTurtle <e.russell1792@gmail.com>
2018-07-10 21:24:49 -0500

    moved google API logic to index.js - still not dynamic

--- public/index.js
+++ public/index.js
@@ -97,10 +97,27 @@ var TripsShowPage = {
         this.trip = response.data;
       }.bind(this));
   },
-  methods: {},
-  computed: {}
-};
+  methods: {
+    initMap: function() {
+        // MAP OPTIONS
+        var options = {
+        zoom: 8,
+        center: {lat: 42.3601, lng: -71.0589}
+      };
+      // CREATING NEW MAP
+      var map = new google.maps.Map(document.getElementById('map'), options);
 
+      var marker = new google.maps.Marker({
+        position:{lat:42.4668, lng:-70.9495},
+        map:map
+      });
+    },
+  },
+  computed: {},
+  mounted: function() {
+    this.initMap();
+  }
+};
 
 var TripsNewPage = {
   template: "#trips-new-page",
